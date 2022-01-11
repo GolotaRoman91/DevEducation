6f4ceec8295a74e9e093691834496da481cd3b64
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2022-01-11 10:48:35 +0200

    node

--- backend/constants/routes.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable linebreak-style */
-module.exports = {
-  GET_ESTONIA_DATA: '/get-data-estonia',
-  GET_POLAND_DATA: '/get-data-poland',
-  GET_NETHERLANDS_DATA: '/get-data-netherlands',
-  GET_CANADA_DATA: '/get-data-canada',
-};
